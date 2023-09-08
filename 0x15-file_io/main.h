69e5b54ae9c8578fe4ddb651261942cf5ef796d9
KThadDeus <thaddeuskobina20@gmail.com>
2023-09-07 19:47:42 -0700

    C- file i/o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-ssize_t read_textfile(const char *filename, size_t letters);
+void exit_with_error(int code, const char *message);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void print_error(const char *message);
 
-#endif
+#endif /* MAIN_H */
