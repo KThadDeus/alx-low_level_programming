4b8df0bf977ef910aa3a2f72c164f1379a2db706
KThadDeus <thaddeuskobina20@gmail.com>
2023-09-07 19:53:10 -0700

    C- file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,35 +4,41 @@
 #include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL)
-        return 0;
+    if (filename == NULL) {
+        return 0; // If filename is NULL, return 0 as specified.
+    }
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+    int file_descriptor = open(filename, O_RDONLY); // Open the file for reading.
 
-    char *buffer = malloc(sizeof(char) * letters);
+    if (file_descriptor == -1) {
+        return 0; // If the file can't be opened, return 0.
+    }
+
+    char *buffer = malloc(letters); // Allocate memory for the buffer.
+
     if (buffer == NULL) {
-        close(fd);
-        return 0;
+        close(file_descriptor);
+        return 0; // If memory allocation fails, return 0.
     }
 
-    ssize_t bytes_read = read(fd, buffer, letters);
+    ssize_t bytes_read = read(file_descriptor, buffer, letters); // Read from the file.
+
     if (bytes_read == -1) {
-        close(fd);
         free(buffer);
-        return 0;
+        close(file_descriptor);
+        return 0; // If reading from the file fails, return 0.
     }
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read); // Write to standard output.
+
     if (bytes_written == -1 || bytes_written != bytes_read) {
-        close(fd);
         free(buffer);
-        return 0;
+        close(file_descriptor);
+        return 0; // If writing fails or doesn't write expected bytes, return 0.
     }
 
-    close(fd);
     free(buffer);
+    close(file_descriptor);
 
-    return bytes_written;
+    return bytes_read; // Return the actual number of letters read and printed.
 }
